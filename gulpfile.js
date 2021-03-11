97a84fcfbd19b44ff35b1644757966318cf5f7ca
TrueNevada <ricefieldsevilsamurai@gmail.com>
2021-03-11 23:18:28 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const sourcemap = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  return gulp.src('sass/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('build/css/'))
+    .pipe(server.stream());
+});
+
+gulp.task('html', function () {
+  return gulp.src('*.html')
+    .pipe(gulp.dest('build/'))
+    .pipe(server.stream());
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('*.html', gulp.series('html'));
+});
