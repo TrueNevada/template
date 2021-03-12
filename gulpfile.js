3381703f040fb184e22424288186b65d78596998
nik <nikolai.kotikov@gmail.com>
2021-03-12 12:59:51 +0300

    build: :hammer: fix build
    Make sure that all assets are available in 'build' folder before serving them

--- gulpfile.js
+++ gulpfile.js
@@ -31,3 +31,13 @@ gulp.task('serve', function () {
   gulp.watch('sass/**/*.scss', gulp.series('sass'));
   gulp.watch('*.html', gulp.series('html'));
 });
+
+/*
+Browser Sync - это веб сервер, построенный на базе фреймворка Express
+Сервер в самом простом случае работает так: получает запрос и по умолчанию
+ищет index.html, если не находит - то возвращает ошибку 404, что и происходило.
+Ты запускал сервер, но в папке build не было index.html на тот момент, поэтому
+он отработал так, как и должен - отдал ошибку 404.
+*/
+
+gulp.task("start", gulp.series("sass", "html", "serve"))
